8e50bf7948f49a9b458c8612492fd7667b7459e2
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2010-05-27 16:21:20 -0430

    Cambios de estilo, imágenes y javascript

--- canaima.aponwaotheme/canaima/aponwaotheme/browser/javascripts/jflow.js
+++ canaima.aponwaotheme/canaima/aponwaotheme/browser/javascripts/jflow.js
@@ -1,7 +1,178 @@
 /* Copyright (c) 2008 Kean Loong Tan http://www.gimiti.com/kltan
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * jFlow
- * Version: 1.1 (May 22, 2008)
+ * Version: 1.2 (July 7, 2008)
  * Requires: jQuery 1.2+
  */
-(function(A){A.fn.jFlow=function(D){var E=A.extend({},A.fn.jFlow.defaults,D);var F=0;var B=A(".jFlowControl").length;A(this).find(".jFlowControl").each(function(G){A(this).click(function(){A(".jFlowControl").removeClass("jFlowSelected");A(this).addClass("jFlowSelected");var H=Math.abs(F-G);A(E.slides).animate({marginLeft:"-"+(G*A(E.slides).find(":first-child").width()+"px")},E.duration*(H));F=G})});A(E.slides).before('<div id="jFlowSlide"></div>').appendTo("#jFlowSlide");A(E.slides).find("div").each(function(){A(this).before('<div class="jFlowSlideContainer"></div>').appendTo(A(this).prev())});A(".jFlowControl").eq(F).addClass("jFlowSelected");var C=function(G){A("#jFlowSlide").css({position:"relative",width:E.width,height:E.height,overflow:"hidden"});A(E.slides).css({position:"relative",width:A("#jFlowSlide").width()*A(".jFlowControl").length+"px",height:A("#jFlowSlide").height()+"px",overflow:"hidden"});A(E.slides).children().css({position:"relative",width:A("#jFlowSlide").width()+"px",height:A("#jFlowSlide").height()+"px","float":"left"});A(E.slides).css({marginLeft:"-"+(F*A(E.slides).find(":first-child").width()+"px")})};C();A(window).resize(function(){C()});A(".jFlowPrev").click(function(){if(F>0){F--}else{F=B-1}A(".jFlowControl").removeClass("jFlowSelected");A(E.slides).animate({marginLeft:"-"+(F*A(E.slides).find(":first-child").width()+"px")},E.duration);A(".jFlowControl").eq(F).addClass("jFlowSelected")});A(".jFlowNext").click(function(){if(F<B-1){F++}else{F=0}A(".jFlowControl").removeClass("jFlowSelected");A(E.slides).animate({marginLeft:"-"+(F*A(E.slides).find(":first-child").width()+"px")},E.duration);A(".jFlowControl").eq(F).addClass("jFlowSelected")})};A.fn.jFlow.defaults={easing:"swing",duration:400,width:"100%"}})(jQuery);
+ 
+(function($) {
+
+	$.fn.jFlow = function(options) {
+		var opts = $.extend({}, $.fn.jFlow.defaults, options);
+		var randNum = Math.floor(Math.random()*11);
+		var jFC = opts.controller;
+		var jFS =  opts.slideWrapper;
+		var jSel = opts.selectedWrapper;
+
+		var cur = 0;
+		var timer;
+		var maxi = $(jFC).length;
+		// sliding function
+		var slide = function (dur, i) {
+			$(opts.slides).children().css({
+				overflow:"hidden"
+			});
+			$(opts.slides + " iframe").hide().addClass("temp_hide");
+			$(opts.slides).animate({
+				marginLeft: "-" + (i * $(opts.slides).find(":first-child").width() + "px")
+				},
+				opts.duration*(dur),
+				opts.easing,
+				function(){
+					$(opts.slides).children().css({
+						overflow:"hidden"
+					});
+					$(".temp_hide").show();
+				}
+			);
+			
+		}
+		$(this).find(jFC).each(function(i){
+			$(this).click(function(){
+				dotimer();
+				if ($(opts.slides).is(":not(:animated)")) {
+					$(jFC).removeClass(jSel);
+					$(this).addClass(jSel);
+					var dur = Math.abs(cur-i);
+					slide(dur,i);
+					cur = i;
+				}
+			});
+		});	
+		
+		$(opts.slides).before('<div id="'+jFS.substring(1, jFS.length)+'"></div>').appendTo(jFS);
+		
+		$(opts.slides).find("div").each(function(){
+			$(this).before('<div class="jFlowSlideContainer"></div>').appendTo($(this).prev());
+		});
+		
+		//initialize the controller
+		$(jFC).eq(cur).addClass(jSel);
+		
+		var resize = function (x){
+			$(jFS).css({
+				position:"relative",
+				width: opts.width,
+				height: opts.height,
+				overflow: "hidden"
+			});
+			//opts.slides or #mySlides container
+			$(opts.slides).css({
+				position:"relative",
+				width: $(jFS).width()*$(jFC).length+"px",
+				height: $(jFS).height()+"px",
+				overflow: "hidden"
+			});
+			// jFlowSlideContainer
+			$(opts.slides).children().css({
+				position:"relative",
+				width: $(jFS).width()+"px",
+				height: $(jFS).height()+"px",
+				"float":"left",
+				overflow:"hidden"
+			});
+			
+			$(opts.slides).css({
+				marginLeft: "-" + (cur * $(opts.slides).find(":eq(0)").width() + "px")
+			});
+		}
+		
+		// sets initial size
+		resize();
+
+		// resets size
+		$(window).resize(function(){
+			resize();						  
+		});
+		
+		$(opts.prev).click(function(){
+			dotimer();
+			doprev();
+			
+		});
+		
+		$(opts.next).click(function(){
+			dotimer();
+			donext();
+			
+		});
+		
+		var doprev = function (x){
+			if ($(opts.slides).is(":not(:animated)")) {
+				var dur = 1;
+				if (cur > 0)
+					cur--;
+				else {
+					cur = maxi -1;
+					dur = cur;
+				}
+				$(jFC).removeClass(jSel);
+				slide(dur,cur);
+				$(jFC).eq(cur).addClass(jSel);
+			}
+		}
+		
+		var donext = function (x){
+			if ($(opts.slides).is(":not(:animated)")) {
+				var dur = 1;
+				if (cur < maxi - 1)
+					cur++;
+				else {
+					cur = 0;
+					dur = maxi -1;
+				}
+				$(jFC).removeClass(jSel);
+				$(jFS).fadeOut("fast");
+				slide(dur, cur);
+				$(jFS).fadeIn("slow");
+				$(jFC).eq(cur).addClass(jSel);
+			}
+		}
+		
+		var dotimer = function (x){
+			if((opts.auto) == true) {
+				if(timer != null) 
+					clearInterval(timer);
+			    
+        		timer = setInterval(function() {
+	                	$(opts.next).click();
+						}, 4000);
+			}
+		}
+
+		dotimer();
+		
+		//Pause/Resume at hover
+		$(opts.slides).hover(
+			function() {
+				clearInterval(timer);
+			},
+			function() {
+				dotimer();
+			}
+		);
+	};
+	
+	$.fn.jFlow.defaults = {
+		controller: ".jFlowControl", // must be class, use . sign
+		slideWrapper : "#jFlowSlide", // must be id, use # sign
+		selectedWrapper: "jFlowSelected", // just pure text, no sign
+		auto: false,
+		easing: "swing",
+		duration: 400,
+		width: "100%",
+		prev: ".jFlowPrev", // must be class, use . sign
+		next: ".jFlowNext" // must be class, use . sign
+	};
+	
+})(jQuery);
--- canaima.aponwaotheme/canaima/aponwaotheme/browser/javascripts/scripts.js
+++ canaima.aponwaotheme/canaima/aponwaotheme/browser/javascripts/scripts.js
@@ -1,12 +1,21 @@
 jQuery(function() {
+	
 	jQuery("#menu ul").lavaLamp({
 		fx: "easeOutBack",
 		speed: 700
     });
+    
+    jQuery("#controller").jFlow({
+		slides: ".slides",
+		width: "960px",
+		height: "210px",
+controller: ".jFlowControl", // must be class, use . sign
+auto: true,     //auto change slide, default true
+duration: 700,
+prev: ".jFlowPrev", // must be class, use . sign
+next: ".jFlowNext" // must be class, use . sign
+	});
+    
+    jQuery('#twitter_msg').twitterize('CanaimaLinux');
+    
 });
-
-jQuery(function() {jQuery("div#controller").jFlow({slides: ".slides",width: "960px",height: "210px"});});
-
-
-jQuery(function() {jQuery('#twitter_msg').twitterize('CanaimaLinux');});
-           
