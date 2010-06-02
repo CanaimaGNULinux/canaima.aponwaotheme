28a11667b8c6089fae04175b664d4eb0c8a49e37
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2010-06-01 20:31:36 -0430

    Modificación de archivos de documentación, bugs menores corregidos.

--- canaima.aponwaotheme/setup.py
+++ canaima.aponwaotheme/setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Éste módulo contiene la herramienta de configuración del tema Canaima Aponwao
+Éste módulo contiene la herramienta de configuración de la instalación del tema Canaima Aponwao
 """
 import os
 from setuptools import setup, find_packages
@@ -8,14 +8,12 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-
-
 setup(name='canaima.aponwaotheme',
-      version='0.9',
-      description="Versión instalable del tema Canaima Aponwao",
-      long_description=open(os.path.join("docs", "README")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      version='0.9.1',
+      description='Tema Canaima Aponwao para Plone, basado en la Metadistribución venezolana "Canaima GNU/Linux"',
+      long_description="\n\n" + open(os.path.join("docs", "README")).read() + "\n\n" +
+                       open(os.path.join("docs", "INSTALL")).read() + "\n\n" +
+                       open(os.path.join("docs", "CHANGELOG")).read(),
       classifiers=[
         'Framework :: Plone',
         'Intended Audience :: Developers',
@@ -23,21 +21,21 @@ setup(name='canaima.aponwaotheme',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
       keywords='web zope plone theme canaima gnu linux aponwao cms',
-      author='Equipo de Desarrollo de Canaima GNU/Linux',
+      author='Equipo de Desarrollo de Canaima GNU/Linux\n\t\t\tLuis A. Martinez F.\n\t\t\tLeonardo J. Caballero G.',
       author_email='desarrolladores@canaima.softwarelibre.gob.ve',
       url='http://canaima.softwarelibre.gob.ve/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['canaima', 'canaima.aponwaotheme',],
       include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools'],
-      setup_requires = ["setuptools",],
-      theme_vars = {
-          'skinname'          : 'Tema Canaima Aponwao para Plone',
+      zip_safe=True,
+      install_requires=['setuptools',],
+      setup_requires=['setuptools',],
+      theme_vars={
+          'skinname'          : 'Canaima Aponwao',
           'skinbase'          : 'Plone Default',
           'namespace_package' : 'canaima',
           'package'           : 'aponwaotheme',
-          'used_subtemplates' : '',
       },
+      platform="Plataforma de Desarrollo Canaima GNU/Linux",
       )
